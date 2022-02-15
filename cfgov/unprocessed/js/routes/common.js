05d1a92bbe8cf0ad2e08e4e85e1502f1263b8687
Charles Werner <charles.werner@cfpb.gov>
2022-02-15 10:05:02 -0500

    -First pass

--- cfgov/unprocessed/js/routes/common.js
+++ cfgov/unprocessed/js/routes/common.js
@@ -8,9 +8,43 @@
 import Footer from '../organisms/Footer.js';
 import Header from '../organisms/Header.js';
 
-const header = new Header( document.body );
+const header = new Header(document.body);
 // Initialize header by passing it reference to global overlay atom.
-header.init( document.body.querySelector( '.a-overlay' ) );
+header.init(document.body.querySelector('.a-overlay'));
 
-const footer = new Footer( document.body );
+const footer = new Footer(document.body);
 footer.init();
+
+
+/**
+ * Chuck's nonsense.
+ */
+function hTest() {
+  let h2b = false;
+  let h3b = false;
+
+  function checkBold(element) {
+    var fontWeight = getComputedStyle( element ).fontWeight;
+    if (Number(fontWeight) > 400 || fontWeight === 'bold' || fontWeight === 'bolder') {
+      return true;
+    }
+    return false;
+  }
+
+  function toggleBold(tag) {
+    document.querySelectorAll(tag)
+      .forEach(header => {
+        if ( checkBold( header) === false ) {
+          header.style.fontWeight = 'bold';
+        } else {
+          header.style.fontWeight = 'normal';
+        }
+      })
+  }
+
+  document.querySelector('#toggle-h2').addEventListener('click', ele => { toggleBold( 'h2' ) } );
+  document.querySelector('#toggle-h2').addEventListener('click', ele => { toggleBold( 'h3' ) } );
+
+}
+
+hTest();
