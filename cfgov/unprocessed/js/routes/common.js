3f825100a37aa6e49bece97646fd2a929ba8561a
Charles Werner <charles.werner@cfpb.gov>
2022-02-16 09:23:05 -0500

    - Better buttons

--- cfgov/unprocessed/js/routes/common.js
+++ cfgov/unprocessed/js/routes/common.js
@@ -20,8 +20,15 @@ footer.init();
  * Chuck's nonsense.
  */
 function hTest() {
-  let h2b = false;
-  let h3b = false;
+  let h2b = 300;
+  let h3b = 300;
+  let h1selector = 'h1:not( .h2, .h3, .h4, .h5, .h6 ), .h1';
+  let h2selector = 'h2:not( .h1, .h3, .h4, .h5, .h6 ), .h2';
+  let h3selector = 'h3:not( .h1, .h2, .h4, .h5, .h6 ), .h3';
+  let h4selector = 'h4:not( .h1, .h2, .h3, .h5, .h6 ), .h4';
+  let h5selector = 'h5:not( .h1, .h2, .h3, .h4, .h6 ), .h5';
+  let h6selector = 'h6:not( .h1, .h2, .h3, .h4, .h5 ), .h6';
+
 
   function checkBold(element) {
     var fontWeight = getComputedStyle( element ).fontWeight;
@@ -31,19 +38,60 @@ function hTest() {
     return false;
   }
 
-  function toggleBold(tag) {
-    document.querySelectorAll(tag)
-      .forEach(header => {
-        if ( checkBold( header) === false ) {
-          header.style.fontWeight = 'bold';
-        } else {
-          header.style.fontWeight = 'normal';
-        }
-      })
+  function bolderize( tag, selector ) {
+    let element = document.querySelector( selector );
+    let button = document.querySelector( '#bolder-' + tag );
+    let fontWeight = 'N/A';
+    if ( element !== null ) {
+      fontWeight = getComputedStyle( element ).fontWeight;
+      if ( fontWeight === 'bold' || fontWeight === 'bolder' ) fontWeight = '700';
+      fontWeight = Number( fontWeight ) + 100;
+      if ( fontWeight > 900 ) fontWeight = 100;
+
+      document.querySelectorAll( selector )
+        .forEach( element => {
+          element.style.fontWeight = fontWeight;
+        })
+    } else {
+      if ( button !== null ) button.innerText = tag + ' weight: N/A';
+    }
+
+    if ( button !== null ) {
+      button.innerText = tag + ' weight: ' + fontWeight;
+    }
+
   }
 
-  document.querySelector('#toggle-h2').addEventListener('click', ele => { toggleBold( 'h2' ) } );
-  document.querySelector('#toggle-h2').addEventListener('click', ele => { toggleBold( 'h3' ) } );
+  document.querySelector('#bolder-h1').addEventListener('click', ele => { bolderize( 'h1', h1selector ); } );
+  document.querySelector('#bolder-h2').addEventListener('click', ele => { bolderize( 'h2', h2selector ); } );
+  document.querySelector('#bolder-h3').addEventListener('click', ele => { bolderize( 'h3', h3selector ); } );
+  document.querySelector('#bolder-h4').addEventListener('click', ele => { bolderize( 'h4', h4selector ); } );
+
+  document.addEventListener( 'DOMContentLoaded', function() {
+    const headers = {
+      'h1': { selector: h1selector },
+      'h2': { selector: h2selector },
+      'h3': { selector: h3selector },
+      'h4': { selector: h4selector }
+    };
+
+    for ( const key in headers) {
+      let element = document.querySelector( headers[key].selector );
+      let button = document.querySelector( '#bolder-' + key );
+      if ( button === null ) {
+        console.log( 'null found: ' + key );
+      } else if ( element !== null ) {
+        let fontWeight = getComputedStyle( element ).fontWeight;
+        if ( fontWeight === 'bold' || fontWeight === 'bolder' ) fontWeight = '700';
+
+        document.querySelector( '#bolder-' + key ).innerText = key + ' weight: ' + fontWeight;
+      } else {
+        let button = document.querySelector( '#bolder-' + key );
+        if ( button !== null ) button.innerText = key + ' weight: ' + fontWeight;
+      }
+
+    };
+  });
 
 }
 
